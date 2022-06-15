3b046720467323703e03601999e03b5c2b06d96a
HaolinZhong <hz2771@cumc.columbia.edu>
2022-06-15 15:12:59 -0400

    Implement Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/guru/spring/myspringpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package guru.spring.myspringpetclinic.services;
+
+import guru.spring.myspringpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}

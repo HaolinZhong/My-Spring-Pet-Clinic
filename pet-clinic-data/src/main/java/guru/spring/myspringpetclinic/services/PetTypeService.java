827cf2b37690177145e97c80c5e5aedc4d68591d
HaolinZhong <hz2771@cumc.columbia.edu>
2022-06-18 22:32:58 -0400

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/guru/spring/myspringpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.spring.myspringpetclinic.services;
+
+import guru.spring.myspringpetclinic.model.PetType;
+import guru.spring.myspringpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}

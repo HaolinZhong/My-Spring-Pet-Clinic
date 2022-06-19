723475a241678c337dc163daadb8348ba2aba465
HaolinZhong <hz2771@cumc.columbia.edu>
2022-06-19 00:55:34 -0400

    fixed broken links. closes #41

--- pet-clinic-web/src/main/java/guru/spring/myspringpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/spring/myspringpetclinic/controllers/IndexController.java
@@ -10,6 +10,11 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/oups")
+    public String oupsHandler() {
+        return "notimplemented";
+    }
 }
 
 

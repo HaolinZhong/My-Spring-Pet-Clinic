4868d37cb1ae5f1525a2950635866979af0f97f3
HaolinZhong <hz2771@cumc.columbia.edu>
2022-06-20 00:06:05 -0400

    Added Visit Entity. closes #38.

--- pet-clinic-data/src/main/java/guru/spring/myspringpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/spring/myspringpetclinic/model/Visit.java
@@ -1,10 +1,20 @@
 package guru.spring.myspringpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
+
+    @Column
     private LocalDate date;
+
+    @Column
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {

d8fc09e65ea1e7d1e94c6e20b0bde555e3fb03cc
HaolinZhong <hz2771@cumc.columbia.edu>
2022-06-20 18:02:52 -0400

    added PetType SD JPA service. closes #42.

---
+++ pet-clinic-data/src/main/java/guru/spring/myspringpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,44 @@
+package guru.spring.myspringpetclinic.services.springdatajpa;
+
+import guru.spring.myspringpetclinic.model.Vet;
+import guru.spring.myspringpetclinic.repositories.VetRepository;
+import guru.spring.myspringpetclinic.services.VetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+}

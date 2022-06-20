b85e0b0f2a60d9cfdf8de16ed5cc7769389059b4
HaolinZhong <hz2771@cumc.columbia.edu>
2022-06-20 19:07:08 -0400

    Implemented JPA visit service. Added to Data loader. closes #43.

--- pet-clinic-data/src/main/java/guru/spring/myspringpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/spring/myspringpetclinic/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package guru.spring.myspringpetclinic.services.map;
 
 import guru.spring.myspringpetclinic.model.Visit;
 import guru.spring.myspringpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
 

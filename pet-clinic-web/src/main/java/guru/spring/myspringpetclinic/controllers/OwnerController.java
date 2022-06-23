c462d19ec159cc6edab9c46d1b6f93e5a3510b71
HaolinZhong <hz2771@cumc.columbia.edu>
2022-06-23 16:17:50 -0400

    fixed find by like. closes #58.

--- pet-clinic-web/src/main/java/guru/spring/myspringpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/spring/myspringpetclinic/controllers/OwnerController.java
@@ -40,7 +40,7 @@ public class OwnerController {
     @GetMapping
     public String processFindForm(Owner owner, BindingResult result, Model model) {
         if (owner.getLastName() == null) owner.setLastName("");
-        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner> results = ownerService.findAllByLastNameLike("%" + owner.getLastName() + "%");
 
         if (results.isEmpty()) {
             result.rejectValue("lastName", "notFound", "not found");

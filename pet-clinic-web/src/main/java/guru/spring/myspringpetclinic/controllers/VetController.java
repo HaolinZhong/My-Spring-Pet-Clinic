b13493b07902c10ffcf3bd9720604ee4cd7b04f0
HaolinZhong <hz2771@cumc.columbia.edu>
2022-06-24 21:07:37 -0400

    added json endpoint for vets. closes #61.

--- pet-clinic-web/src/main/java/guru/spring/myspringpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/spring/myspringpetclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package guru.spring.myspringpetclinic.controllers;
 
+import guru.spring.myspringpetclinic.model.Vet;
 import guru.spring.myspringpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -19,4 +23,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
+    }
+
 }
